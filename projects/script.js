a2fcafa5de4ed154d1e84dd71af6ecc326e789f8
Vishalgethub <126494308+Vishalgethub@users.noreply.github.com>
2023-03-19 11:48:26 +0530

    error fix

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Por";
+            document.title = "Projects | Portfolio Vishal";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
